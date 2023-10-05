913251e77149560cfb64e92508e53a7bb3a156e5
dmitriev <nafoko@mail.ru>
2023-10-05 10:56:44 +0300

    task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -4,23 +4,16 @@
 // 2, 4 -> 16
 
 
-class Program
+
+
+Console.Write("Введите число   : ");
+int num = Convert.ToInt32(Console.ReadLine());  //число, которое нужно возвести в степень
+Console.Write("Введите степень : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int num_n = 1; //число в степени
+
+for (int i = 0; i < n; i++)
 {
-    public static int Main()
-    {
-        Console.Write("Введите число   : ");
-        int num = int.Parse(Console.ReadLine());  //число, которое нужно возвести в степень
-        Console.Write("Введите степень : ");
-        int n = int.Parse(Console.ReadLine());   //степень числа
-
-        int num_n = 1; //число в степени
-
-        for (int i = 0; i < n; i++)
-        {
-            num_n = num_n * num;
-        }
-        Console.WriteLine("{0} ^ {1} = {2}", num, n, num_n);
-        Console.ReadKey();
-        return 0;
-    }
+    num_n = num_n * num;
 }
+Console.WriteLine("{0} ^ {1} = {2}", num, n, num_n);
