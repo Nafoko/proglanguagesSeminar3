a991100af6cbca6f592514d19640d8d8c6b44a4f
dmitriev <nafoko@mail.ru>
2023-10-09 20:26:11 +0300

    task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -3,9 +3,37 @@
 public class Answer
 {
     static bool IsPalindrome(int number)
-    
-      if (number < 1000 || number > 99999)
+    {
+        if (number < 10000 || number >= 100000)
         {
-           Console.WriteLine("Число не пятизначное");
-          return false;
+            Console.WriteLine("Число не пятизначное");
+            return false;
         }
+
+        int reverse = 0;
+        int temp = number;
+        while (temp > 0)
+        {
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+        return reverse == number;
+    }
+
+    static public void Main(string[] args)
+    {
+        int number;
+
+        if (args.Length >= 1)
+        {
+            number = int.Parse(args[0]);
+        }
+        else
+        {
+            number = 645464;
+        }
+
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
