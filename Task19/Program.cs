f17039013417f516a6eeea9fb6f7298c9414a48a
dmitriev <nafoko@mail.ru>
2023-10-09 19:36:38 +0300

    task19

---
+++ Task19/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number)
+    
+      if (number < 1000 || number > 99999)
+        {
+           Console.WriteLine("Число не пятизначное");
+          return false;
+        }
