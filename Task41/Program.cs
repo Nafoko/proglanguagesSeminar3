1af0b5a6fda7394d5872a894b1c8ebe4c28ed827
dmitriev <nafoko@mail.ru>
2023-10-12 18:42:29 +0300

    task41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("$Ввести информацию:"  );
+
+int a = int.Parse(Console.ReadLine());
+
+char input = char.Parse(Console.ReadLine());
+
+int b = int.Parse(Console.ReadLine());
+
