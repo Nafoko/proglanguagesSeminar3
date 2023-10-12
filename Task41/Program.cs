672556a4bc1b4e04da0d108a8df0b1b5b7e35bed
dmitriev <nafoko@mail.ru>
2023-10-12 18:51:30 +0300

    task41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -3,11 +3,47 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.Write("$Ввести информацию:"  );
+// Console.Write("Введите элементы(через пробел): ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
 
-int a = int.Parse(Console.ReadLine());
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         count++;
+//     }
+// }
 
-char input = char.Parse(Console.ReadLine());
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
 
-int b = int.Parse(Console.ReadLine());
 
+Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[m];
+
+void mas(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива ");
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+}
+
+int kol(int[] Array)
+{
+    int i = 0;
+    int sum = 0;
+    while (i < Array.Length)
+    {
+        if (Array[i] > 0)
+            sum = sum + 1;
+        i = i + 1;
+    }
+    return sum;
+}
+
+mas(m);
+Console.Write($"\n Чисел больше нуля: {kol(Array)}");
