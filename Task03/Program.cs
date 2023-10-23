d29b2fb345ccc8b9fa3991fbec6a94e982cb63ea
dmitriev <nafoko@mail.ru>
2023-10-23 20:05:36 +0300

    task51

---
+++ Task03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу котоаря принимает на ввод два числа 
+// и выводить является ли первое число кратным второму.
+// Если первое число не кратно второму, то программа выводит отстаток от деления.
+
+
+int Remainder(int firstPar, int SecondPar)
+{
+    return firstPar % SecondPar;
+}
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+int remainder = Remainder(firstNumber, secondNumber);
+if (remainder == 0) Console.WriteLine("Кратно");
+else Console.WriteLine($"не кратно, остаток {remainder}");
---
+++ Task05/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход два числа 
+// и проверяет, является ли ондно число квадратом второго 
+
+bool IsOneSquareOfTheOther (int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
+Console.WriteLine ($"Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsOneSquareOfTheOther(number1, number2);
+Console.WriteLine(result ? "да" : "нет");
+
